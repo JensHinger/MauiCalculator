a66e6905077767bdaf66240ea0c343dfe1371295
JensHinger <hinger.jens@gmail.com>
2023-09-18 17:34:05 +0200

    added solve equation logic

--- Calculator/Views/MainPage.xaml.cs
+++ Calculator/Views/MainPage.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using Calculator.Models;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Calculator.Views;
 
 public partial class MainPage : ContentPage
 {
+    static string[] operators = { "+", "-", "*", "/" };
+    static string joinedOperators = string.Join("", operators);
+
     public MainPage()
     {
         InitializeComponent();
@@ -15,8 +21,7 @@ public partial class MainPage : ContentPage
     {
         string extension = ((Button)sender).Text;
         string lastPart = equation.Text.Split(' ').Last();
-        string[] operators = { "+", "-", "*", "/" };
-        string joinedOperators = string.Join("", operators);
+        
 
         // No number should have two "," and no comma should be before a number
         if (extension == "," && (lastPart.Contains(',') || joinedOperators.Contains(lastPart))) return;
@@ -51,12 +56,45 @@ public partial class MainPage : ContentPage
     private void ClickedSolve(object sender, EventArgs e)
     {
         // Equation will always either be only a number (20,55) or
-        // a equation with two values and one operator(20,55 + 10,5) 
-        SolveEquation();
+        // a equation with two values and one operator(20,55 + 10,5)
+        string lastOperator = Regex.Replace(equation.Text.Split(' ').Last(), @"\s+", "");
+
+        if (joinedOperators.Contains(lastOperator))
+        {
+            return;
+        }
+        else
+        {
+            SolveEquation();
+        } 
     }
 
     private void SolveEquation()
     {
-        equation.Text = "Solved";
+        // does not work with negativ numbers currently
+        String[] equation_parts = equation.Text.Split(" ");
+        
+
+        float first_number = float.Parse(equation.Text.Split(' ')[0], CultureInfo.InvariantCulture.NumberFormat);
+
+        string operation = equation.Text.Split(' ')[1];
+        operation = Regex.Replace(operation, @"\s+", "");
+
+        float second_number = float.Parse(equation.Text.Split(' ').Last(), CultureInfo.InvariantCulture.NumberFormat);
+
+        float solution = DoOperation(first_number, operation, second_number);
+        equation.Text = solution.ToString();
+    }
+
+    private float DoOperation(float num1, string operation, float num2)
+    {
+        switch (operation)
+        {
+            case "+": return num1 + num2;
+            case "-": return num1 - num2;
+            case "*": return num1 * num2;
+            case "/": return num1 / num2;
+            default: return 0.0f;
+        }
     }
 }
